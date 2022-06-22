5e02328e315595c2623f7742e31463b575e27dc5
bientv <bientv@vitalify.asia>
2022-06-22 16:50:19 +0700

    edit main

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 const jsonServer = require('json-server');
+const queryString = require('query-string');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const queryString = require('query-string');
 const middlewares = jsonServer.defaults();
-server.listen(5000,'0.0.0.0', () => {
-  console.log("Express server listening on port %d in %s mode", server.address().port, server.settings.env);
-});
+
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
 
@@ -21,35 +19,45 @@ server.use((req, res, next) => {
   if (req.method === 'POST') {
     req.body.createdAt = Date.now();
     req.body.updatedAt = Date.now();
-  }
-  if (req.method === 'PATCH') {
+  } else if (req.method === 'PATCH') {
     req.body.updatedAt = Date.now();
   }
+
   // Continue to JSON Server router
   next();
 });
+
+// Custom output for LIST with pagination
 router.render = (req, res) => {
-  // console.log(res.query);
-
+  // Check GET with pagination
+  // If yes, custom output
   const headers = res.getHeaders();
-  const totalCountHeaders = headers['x-total-count'];
 
-  // console.log(totalCountHeaders);
-  if (req.method === 'GET' && totalCountHeaders) {
-    console.log(req);
+  const totalCountHeader = headers['x-total-count'];
+  if (req.method === 'GET' && totalCountHeader) {
     const queryParams = queryString.parse(req._parsedUrl.query);
+
     const result = {
       data: res.locals.data,
       pagination: {
         _page: Number.parseInt(queryParams._page) || 1,
         _limit: Number.parseInt(queryParams._limit) || 10,
-        _totalRows: totalCountHeaders,
+        _totalRows: Number.parseInt(totalCountHeader),
       },
     };
+
     return res.jsonp(result);
   }
-  return res.jsonp(res.locals.data);
+
+  // Otherwise, keep default behavior
+  res.jsonp(res.locals.data);
 };
+
 // Use default router
 server.use('/api', router);
 
+// Start server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log('JSON Server is running');
+});
